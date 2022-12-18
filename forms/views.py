1ee732386b51c388d85db17b713a8278f19d7c15
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-18 18:29:20 +0500

    `anonim requests`!

--- forms/views.py
+++ forms/views.py
@@ -8,10 +8,7 @@ def home(request):
 
 # @login_required(login_url='/')
 def NewFormView(request):
-    # if request.user.is_authenticated:
     author = get_object_or_404(CustomUser, username=request.user)
-    # else:
-    #     author = get_object_or_404(CustomUser, username='anonim')
     new_dash = None
     NewForm = CreateFormForm()
     if request.method == 'POST':
@@ -30,8 +27,33 @@ def NewFormView(request):
 
 def SingleView(request, slug):
     single = Form.objects.get(slug=slug)
-    
+    form_q = get_object_or_404(Form, slug=slug)
+    user_r = None
+    if single.anonim_requests == False:
+        if request.user.is_authenticated:
+            user_r = get_object_or_404(CustomUser, username=request.user)
+        else:
+            return redirect('/')
+    else:
+        if request.user.is_authenticated:
+            user_r = get_object_or_404(CustomUser, username=request.user)
+        else:
+            user_r = get_object_or_404(CustomUser, username='anonim')
+    form_requests_count = form_q.request.count()
+    new_request = None
+    if request.method == 'POST':
+        request_form = CreateFormRequestTest(data=request.POST)
+        if request_form.is_valid():
+            new_request = request_form.save(commit=False)
+            new_request.form = form_q
+            new_request.user = user_r
+            new_request.save()
+            return redirect("base:single", slug) # redirect to this url
+    else:
+        request_form = CreateFormRequestTest()
     context = {
-        "single":single
+        "single":single,
+        "form_requests_count":form_requests_count,
+        "request_form":request_form
     }
     return render(request, 'pages/single.html', context)
