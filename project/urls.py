022efdeadd6e70b116a5f1fcc7ea6cb81245475e
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-24 20:57:08 +0500

    static and media

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('forms.urls'))
-]
+    path('', include('forms.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
