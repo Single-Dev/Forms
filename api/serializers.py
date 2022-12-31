e3a39d9d8be55cfea8194123fe1c30704cdf5db0
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-31 16:03:25 +0500

    update users api

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,10 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from forms.models import *
 
-User = get_user_model()
 
 # User API
 class UsersApi(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ("id", "username","email", "first_name", "last_name", "is_organiser", "is_agent",)
+        model = CustomUser
+        # fields = ("id", "username","email", "first_name", "last_name","date_joined","last_login","is_organiser", "is_agent", "user_permissions" )
+        fields = '__all__'
