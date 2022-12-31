113756b4e754d13934e7722c3667f36b437da12e
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-12-31 16:06:09 +0500

    users profile

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'api'
 
 urlpatterns = [
-    path("users/", UsersApiView, name='users')
+    path("users/", UsersApiView, name='users'),
+    path("user/<str:username>/", UserProfileApi, name="user_profile")
 ]
--- api/views.py
+++ api/views.py
@@ -6,10 +6,20 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions, filters, generics
 
 # ----------------------------- Users API ----------------------------- #
+# ----------------------------- All User API
 @api_view(["GET"])
 @permission_classes((permissions.AllowAny, ))
 def UsersApiView(request):
     user = CustomUser.objects.all()
     serializer = UsersApi(user, many=True)
     return Response(serializer.data)
+# ----------------------------- All User API
+# ----------------------------- View User
+@api_view(["GET"])
+@permission_classes((permissions.AllowAny, ))
+def UserProfileApi(request, username):
+    user = CustomUser.objects.get(username=username)
+    serializer = UsersApi(user, many=False)
+    return Response(serializer.data)
+# ----------------------------- View User
 # ----------------------------- Users API ----------------------------- #
