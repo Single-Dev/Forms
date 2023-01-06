f4db0a8f5ec5a9e351e0846021befe6ee2470611
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-01-06 18:20:01 +0500

    update

--- forms/admin.py
+++ forms/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(CustomUser)
+admin.site.register(Profile)
 admin.site.register(Form)
 admin.site.register(FormRequest)
--- forms/models.py
+++ forms/models.py
@@ -11,12 +11,12 @@ class Profile(models.Model):
         verbose_name = "My Profile"
         verbose_name_plural = "Profile"
     custom_user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    image = models.ImageField(default="profile/profile.jpg", upload_to="profile")
+    image = models.ImageField(default="profile/profile_14TGeRN.png", upload_to="profile")
     bio = models.CharField(max_length=100, null=True, blank=True, default="")
     is_online = models.BooleanField(default=False)
     is_verify = models.BooleanField(default=False)
     def __str__(self):
-        return f"id: {self.id}, {self.custom_user}"
+        return f"id: {self.id}; user id: {self.custom_user.id}"
 
 class Form(models.Model):
     title = models.CharField(max_length=50)
