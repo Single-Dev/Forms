86a9a1c458e2abea9af8519417e34ebfbb6f56a1
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-02-12 10:16:29 +0500

    notify better

--- static/js/notify.js
+++ static/js/notify.js
@@ -1,10 +1,11 @@
 let requests = document.querySelectorAll('#requests')
 let response = document.getElementById('response')
+response.style.display = "none"
 if (requests && response) {
     if (requests.length > 0) { 
         document.head.querySelector('title').innerHTML = `notifications (${requests.length})`
     }
     else if (requests.length <= 0) {
-        response.innerHTML = "<h1>Asosiy jild top toza!</h1>"
+        response.style.display = "block"
     }
 }
