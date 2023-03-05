93fff8bf7a23d1c2a064d83e47f98b69c836e9b4
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-03-05 17:17:31 +0500

    added send form

--- site_dashboard/views.py
+++ site_dashboard/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from .forms import *
+User = get_user_model()
 
 def home(request):
     template_name = 'admin/index.html'
+    send_form = SendToEmailForm()
+    if request.method == "POST":
+        send_form = SendToEmailForm(request, data=request.POST)
+        if send_form.is_valid():
+            subject = send_form.cleaned_data.get("subject")
+            message = send_form.cleaned_data.get("message")
+            from_email = send_form.cleaned_data.get("email")
+    
+    recipient_list = User.objects.filter('email')
+    send_mail(subject, message, from_email, recipient_list)
+    context = {
+        "send_form":send_form
+    }
     return render(request, template_name)
