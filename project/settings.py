33157c03f878ae4d07e7ef9fb0bffa62c80a6a59
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-03-17 21:28:07 +0500

    update request view

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d!&rcctok3f%99x*)u34l@q2p%f#a-t++xe(vy%44_0h-@s^br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
